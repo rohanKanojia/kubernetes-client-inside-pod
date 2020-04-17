59e6243e3dc4169d709114ecca6db084b3dc4f7f
Rohan Kumar <rohaan@redhat.com>
2020-04-17 21:40:14 +0530

    Use @autowired for instantiating KubernetesService

--- src/main/java/io/fabric8/examples/kubernetesclientinsidepod/KubernetesClientInsidePodEndpoint.java
+++ src/main/java/io/fabric8/examples/kubernetesclientinsidepod/KubernetesClientInsidePodEndpoint.java
@@ -1,6 +1,7 @@
 package io.fabric8.examples.kubernetesclientinsidepod;
 
 import io.fabric8.examples.kubernetesclientinsidepod.service.KubernetesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +9,11 @@ import java.util.Map;
 
 @RestController
 public class KubernetesClientInsidePodEndpoint {
+    @Autowired
+    KubernetesService service;
+
     @RequestMapping(value = "/", produces = "application/json")
     public Map<String, String> getPods() {
-        KubernetesService service = new KubernetesService();
         return service.getPods();
     }
 }
--- src/main/java/io/fabric8/examples/kubernetesclientinsidepod/service/KubernetesService.java
+++ src/main/java/io/fabric8/examples/kubernetesclientinsidepod/service/KubernetesService.java
@@ -4,9 +4,12 @@ import io.fabric8.kubernetes.api.model.PodList;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientException;
+import org.springframework.stereotype.Service;
+
 import java.util.HashMap;
 import java.util.Map;
 
+@Service
 public class KubernetesService {
     public KubernetesClient client = null;
 
